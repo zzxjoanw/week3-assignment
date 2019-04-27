1105952a74fe102fa49f24793042f2f88bf28c0a
Laura_MS <lmayers3@uw.edu>
2019-04-27 18:08:58 -0400

    add card output

--- ecommerce-ui/src/App.js
+++ ecommerce-ui/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Card from './components/card';
+import airbnbs from './components/airbnbs';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="cardHolder">
+      {
+        Object.keys(airbnbs).map((item, index) => (
+          <Card 
+            key={index}
+            title={airbnbs[item].title}
+            city={airbnbs[item].location.city}
+            country={airbnbs[item].location.country}
+            image={airbnbs[item].image}
+            hostname={airbnbs[item].hostname}
+            isSuperHost={airbnbs[item].isSuperHost}
+            cost={airbnbs[item].payment.cost}
+            cancellation={airbnbs[item].payment.description}
+            starCount={airbnbs[item].rating.stars}
+            reviewCount={airbnbs[item].rating.reviews}>
+          </Card>
+        ))
+      }
     </div>
   );
 }
