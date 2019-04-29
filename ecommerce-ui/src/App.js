e464174f60d302b5deaba821872292a368d9854b
Laura_MS <lmayers3@uw.edu>
2019-04-28 21:50:41 -0400

    add cart to output

--- ecommerce-ui/src/App.js
+++ ecommerce-ui/src/App.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import Card from './components/card';
+import Cart from './components/cart';
 import airbnbs from './components/airbnbs';
 import './App.css';
 
 function App() {
   return (
-    <div id="cardHolder">
-      {
-        Object.keys(airbnbs).map((item, index) => (
-          <Card 
-            key={index}
-            title={airbnbs[item].title}
-            city={airbnbs[item].location.city}
-            country={airbnbs[item].location.country}
-            image={airbnbs[item].image}
-            hostname={airbnbs[item].hostname}
-            isSuperHost={airbnbs[item].isSuperHost}
-            cost={airbnbs[item].payment.cost}
-            cancellation={airbnbs[item].payment.description}
-            starCount={airbnbs[item].rating.stars}
-            reviewCount={airbnbs[item].rating.reviews}>
-          </Card>
-        ))
-      }
-    </div>
+    <>
+      <Cart resCount={3}></Cart>
+      <div id="cardHolder">
+        {
+          Object.keys(airbnbs).map((item, index) => (
+            <Card 
+              key={index}
+              title={airbnbs[item].title}
+              city={airbnbs[item].location.city}
+              country={airbnbs[item].location.country}
+              image={airbnbs[item].image}
+              hostname={airbnbs[item].host.name}
+              isSuperHost={airbnbs[item].isSuperHost}
+              cost={airbnbs[item].payment.cost}
+              cancellation={airbnbs[item].payment.description}
+              starCount={airbnbs[item].rating.stars*20}
+              reviewCount={airbnbs[item].rating.reviews}>
+            </Card>
+          ))
+        }
+      </div>
+    </>
   );
 }
 
